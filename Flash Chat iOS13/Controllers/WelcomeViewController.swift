c04335020ba390d36069b2246e18e43dc317f247
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-06-23 02:52:24 +0530

    implimented the text animation using the loops

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,10 +1,7 @@
 //
 //  WelcomeViewController.swift
 //  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
+
 
 import UIKit
 
@@ -14,7 +11,20 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText{
+            print(charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+        
+        
        
     }
     
